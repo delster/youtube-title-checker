c137658ed1bd6d821bd6acbedf19e5aefe5fcbb9
David Elster <davidandtheweb@gmail.com>
2019-03-25 21:07:13 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Grab references to the DOM elements we'll be listening to.
+  const thumbnailInput = document.querySelector('form .thumbnail.field');
+  const lengthInput = document.querySelector('form .length.field');
+  const titleInput = document.querySelector('form .title.field');
+  const channelInput = document.querySelector('form .channel.field');
+  const viewcountInput = document.querySelector('form .viewcount.field');
+  const uploadagoInput = document.querySelector('form .uploadago.field');
+  const upvotepctInput = document.querySelector('form .upvotepct.field');
+
+
+  // Input onChange Events (when form fields are edited)
+  thumbnailInput.addEventListener("change", e => {});
+  lengthInput.addEventListener("change", e => {});
+  titleInput.addEventListener("change", e => {});
+  channelInput.addEventListener("change", e => {});
+  viewcountInput.addEventListener("change", e => {});
+  uploadagoInput.addEventListener("change", e => {});
+  upvotepctInput.addEventListener("change", e => {});
+
+
+
+})
