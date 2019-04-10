bcce4ecdd135b9d35fe15273e186abaaf28a4192
David Elster <davidandtheweb@gmail.com>
2019-04-10 19:22:17 -0400

    v1.0

--- main.js
+++ main.js
@@ -1,74 +1,72 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Grab references to the DOM elements we'll be listening to.
   const userInputForm = document.querySelector(".userinput form")
-  const thumbnailInput = userInputForm.querySelector(".userinput .thumbnail");
-  const lengthInput = userInputForm.querySelector(".userinput .length");
-  const titleInput = userInputForm.querySelector(".userinput .title");
-  const channelInput = userInputForm.querySelector(".userinput .channel");
-  const viewcountInput = userInputForm.querySelector(".userinput .viewcount");
-  const uploadagoInput = userInputForm.querySelector(".userinput .uploadago");
-  const upvotepctInput = userInputForm.querySelector(".userinput .upvotepct");
+  const thumbnailField = userInputForm.querySelector(".userinput .thumbnail");
+  const lengthField = userInputForm.querySelector(".userinput .length");
+  const titleField = userInputForm.querySelector(".userinput .title");
+  const channelField = userInputForm.querySelector(".userinput .channel");
+  const viewcountField = userInputForm.querySelector(".userinput .viewcount");
+  const uploadagoField = userInputForm.querySelector(".userinput .uploadago");
 
   // Create previews based on our form.
   Desktop(
-    thumbnailInput,
-    channelInput,
-    lengthInput,
-    titleInput,
-    viewcountInput
+    thumbnailField,
+    channelField,
+    lengthField,
+    titleField,
+    viewcountField
   );
   SuggestedHome(
-    thumbnailInput,
-    lengthInput,
-    titleInput,
-    channelInput,
-    viewcountInput,
-    uploadagoInput,
-    upvotepctInput
+    thumbnailField,
+    lengthField,
+    titleField,
+    channelField,
+    viewcountField,
+    uploadagoField,
   );
   SuggestedSidebar(
-    thumbnailInput,
-    lengthInput,
-    titleInput,
-    channelInput,
-    viewcountInput,
-    upvotepctInput
+    thumbnailField,
+    lengthField,
+    titleField,
+    channelField,
+    viewcountField,
   );
 
+  // Title Input: Hard Limit of 100ch
+  titleField.addEventListener( "input", e => {
+    // Grab the children.
+    let input = titleField.querySelector('input');
+    let chLabel = titleField.querySelector('.ch');
+    // Grab the value length.
+    let count = input.value.length;
+    // If it's over the max, truncate it.
+    if(count>100) {
+      // Code Smell: Magic Number for ch limit
+      input.value = input.value.slice(0,99)
+    }
+    // Show the character count in the label below.
+    chLabel.textContent = input.value.length;
+  });
+
   // Prevent Form Submission (just in case..)
-  userInputForm.addEventListener( "submit", e => e.preventDefault() )
+  userInputForm.addEventListener( "submit", e => e.preventDefault() );
 });
 
-const Desktop = (
-  thumbnailInput,
-  channelInput,
-  lengthInput,
-  titleInput,
-  viewcountInput
-) => {
+const Desktop = ( thumbnailField, channelField, lengthField, titleField, viewcountField ) => {
   const desktopEl = document.querySelector(".desktop-full");
   const thumbnailOutput = desktopEl.querySelector(".thumbnail");
   const channelOutput = desktopEl.querySelector(".channel");
   const lengthOutput = desktopEl.querySelector(".length");
   const titleOutput = desktopEl.querySelector(".title");
   const viewcountOutput = desktopEl.querySelector(".viewcount");
-
-  thumbnailInput.addEventListener("input", e => { thumbnailOutput.src = e.target.value; });
-  channelInput.addEventListener("input", e => { channelOutput.textContent = e.target.value; });
-  lengthInput.addEventListener("input", e => { lengthOutput.textContent = e.target.value; });
-  titleInput.addEventListener("input", e => { titleOutput.textContent = e.target.value; });
-  viewcountInput.addEventListener("input", e => { viewcountOutput.textContent = e.target.value; });
+  thumbnailField.addEventListener("input", e => { fileToPreview(thumbnailField.querySelector('input[type=file]'), thumbnailOutput) });
+  channelField.addEventListener("input", e => { channelOutput.textContent = e.target.value; });
+  lengthField.addEventListener("input", e => { lengthOutput.textContent = e.target.value; });
+  titleField.addEventListener("input", e => { titleOutput.textContent = e.target.value; });
+  viewcountField.addEventListener("input", e => { viewcountOutput.textContent = e.target.value; });
 };
 
-const SuggestedHome = (
-  thumbnailInput,
-  lengthInput,
-  titleInput,
-  channelInput,
-  viewcountInput,
-  uploadagoInput,
-  upvotepctInput
-) => {
+const SuggestedHome = ( thumbnailField, lengthField, titleField, channelField, viewcountField, uploadagoField ) => {
   const shomeEl = document.querySelector(".suggested > .home");
   const thumbnailOutput = shomeEl.querySelector(".thumbnail");
   const lengthOutput = shomeEl.querySelector(".length");
@@ -76,37 +74,31 @@ const SuggestedHome = (
   const channelOutput = shomeEl.querySelector(".channel");
   const viewcountOutput = shomeEl.querySelector(".viewcount");
   const uploadagoOutput = shomeEl.querySelector(".uploadago");
-  const upvotepctOutput = shomeEl.querySelector(".upvotepct");
-
-  thumbnailInput.addEventListener("input", e => { thumbnailOutput.src = e.target.value; });
-  lengthInput.addEventListener("input", e => { lengthOutput.textContent = e.target.value; });
-  titleInput.addEventListener("input", e => { titleOutput.textContent = e.target.value; });
-  channelInput.addEventListener("input", e => { channelOutput.textContent = e.target.value; });
-  viewcountInput.addEventListener("input", e => { viewcountOutput.textContent = e.target.value; });
-  uploadagoInput.addEventListener("input", e => { uploadagoOutput.textContent = e.target.value; });
-  upvotepctInput.addEventListener("input", e => { upvotepctOutput.textContent = e.target.value; });
+  thumbnailField.addEventListener("input", e => { fileToPreview(thumbnailField.querySelector('input[type=file]'), thumbnailOutput) });
+  lengthField.addEventListener("input", e => { lengthOutput.textContent = e.target.value; });
+  titleField.addEventListener("input", e => { titleOutput.textContent = e.target.value; $clamp(titleOutput, { clamp: 2 }); });
+  channelField.addEventListener("input", e => { channelOutput.textContent = e.target.value; });
+  viewcountField.addEventListener("input", e => { viewcountOutput.textContent = e.target.value; });
+  uploadagoField.addEventListener("input", e => { uploadagoOutput.textContent = e.target.value; });
 };
 
-const SuggestedSidebar = (
-  thumbnailInput,
-  lengthInput,
-  titleInput,
-  channelInput,
-  viewcountInput,
-  upvotepctInput
-) => {
+const SuggestedSidebar = ( thumbnailField, lengthField, titleField, channelField, viewcountField ) => {
   const ssidebarEl = document.querySelector(".suggested > .sidebar");
   const thumbnailOutput = ssidebarEl.querySelector(".thumbnail");
   const lengthOutput = ssidebarEl.querySelector(".length");
   const titleOutput = ssidebarEl.querySelector(".title");
   const channelOutput = ssidebarEl.querySelector(".channel");
   const viewcountOutput = ssidebarEl.querySelector(".viewcount");
-  const upvotepctOutput = ssidebarEl.querySelector(".upvotepct");
-
-  thumbnailInput.addEventListener("input", e => { thumbnailOutput.src = e.target.value; })
-  lengthInput.addEventListener("input", e => { lengthOutput.textContent = e.target.value; })
-  titleInput.addEventListener("input", e => { titleOutput.textContent = e.target.value; })
-  channelInput.addEventListener("input", e => { channelOutput.textContent = e.target.value; })
-  viewcountInput.addEventListener("input", e => { viewcountOutput.textContent = e.target.value; })
-  upvotepctInput.addEventListener("input", e => { upvotepctOutput.textContent = e.target.value; })
+  thumbnailField.addEventListener("input", e => { fileToPreview(thumbnailField.querySelector('input[type=file]'), thumbnailOutput) })
+  lengthField.addEventListener("input", e => { lengthOutput.textContent = e.target.value; })
+  titleField.addEventListener("input", e => { titleOutput.textContent = e.target.value; $clamp(titleOutput, { clamp: 2 }); })
+  channelField.addEventListener("input", e => { channelOutput.textContent = e.target.value; })
+  viewcountField.addEventListener("input", e => { viewcountOutput.textContent = e.target.value; })
 };
+
+const fileToPreview = (fileInput, imgTag) => {
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+  reader.onload = e => { imgTag.src = e.target.result }
+  reader.readAsDataURL(file);
+}
